fd98fe02f68db18524bdf7bdecce009332bdb0c6
mohanpierce99 <mohan@madstreetden.com>
2020-03-16 22:17:29 +0530

    Client code added

--- seed.js
+++ seed.js
@@ -1,59 +1,42 @@
 var WebTorrent = require('webtorrent-hybrid')
 var client = new WebTorrent();
-const color = require('colors');
+// const color = require('colors');
 
 
 
-function seed(filepath, bar1) {
-  if(bar1){
-    bar1.start(200, 0, {
-      status: 'Created and bootstrapped !'
-    });
-  }
-
-
-  // update the current value in your application..
+function seed(filepath, cb) {
 
   // stop the progress bar
   return new Promise((res, rej) => {
+      cb({status:"Created and bootstrapped !",no:0});
+    bar1.start(200, 0, {
+      status: ''
+    });
     var torrent = client.seed(filepath, {
       announce: ['http://localhost:8000/announce',
         'udp://0.0.0.0:8000',
         'udp://localhost:8000', 'ws://localhost:8000'
       ]
     }, function (torrent) {
-      bar1.update(200, {
-        status: "Wrapping up!"
-      });
-      bar1.stop();
+      cb({status:"Wrapping up!",no:200});
       console.log("\n");
       console.log('Client is seeding \n \n' + torrent.magnetURI.green);
       res(torrent.magnetURI);
     });
 
     torrent.on('infoHash', function () {
-      setTimeout(() => {
-        bar1.update(50, {
-          status: "Info Hash Created"
-        });
-      }, 1000)
+      cb({status:"Info Hash Created",no:50}); //timeout
     });
+
     torrent.on('metadata', function () {
-      setTimeout(() => {
-        bar1.update(100, {
-          status: "Torrent cooked!"
-        });
-      }, 1000);
+      cb({status:"Torrent cooked!"}); //timeout
     })
 
     torrent.on('ready', function () {
-      setTimeout(() => {
-        bar1.update(150);
-      }, 1000)
+      cb({status:"Almost ready !",no:150});
+
       torrent.on('done', function () {
-        setTimeout(() => {
-          bar1.update(190);
-        }, 2000)
+        cb({status:"Almost ready !",no:190});
       })
       torrent.on('download', function () {})
       torrent.on('wire', function () {
